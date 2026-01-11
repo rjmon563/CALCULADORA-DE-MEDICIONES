43d9265aca18bcc72f7b2cbf0b72085a4da1dc03
rjmon563 <153252411+rjmon563@users.noreply.github.com>
2026-01-11 19:59:49 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calculo-medicion-v6';
+const CACHE_NAME = 'calculo-medicion-v7'; // Versión v7 para corregir iconos
 const ASSETS = [
   './',
   './index.html',
@@ -12,17 +12,32 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
 ];
 
+// Instalación: Guardar archivos esenciales en caché
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activación: Borrar todas las cachés antiguas (v1 a v6)
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => {
-    return Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
-  }));
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
+// Estrategia: Intentar red siempre para cambios frescos, si no hay red, usar la caché
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
