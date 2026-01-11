6b3832831831cd0d7cb6ac7cc93227469ef6cc29
rjmon563 <153252411+rjmon563@users.noreply.github.com>
2026-01-11 19:53:20 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'calculo-medicion-v5'; // Actualizado a v5
+const CACHE_NAME = 'calculo-medicion-v6';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
@@ -11,32 +12,17 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
 ];
 
-// Instalación y almacenamiento en caché
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// Limpieza de versiones antiguas (v1 a v4)
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
+  }));
 });
 
-// Estrategia: Red primero para asegurar cambios, caché como respaldo
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
